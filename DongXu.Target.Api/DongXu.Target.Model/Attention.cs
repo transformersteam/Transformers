e4c1a9adb74bf1f574e5d5b41e3a39749893f660
1067014728 <51693060+1067014728@users.noreply.github.com>
2019-06-11 17:22:25 +0800

    欣的

--- DongXu.Target.Api/DongXu.Target.Model/Attention.cs
+++ DongXu.Target.Api/DongXu.Target.Model/Attention.cs
@@ -8,10 +8,29 @@ namespace DongXu.Target.Model
     /// </summary>
     public partial class Attention
     {
+        /// <summary>
+        /// 主键id
+        /// </summary>
         public int AttentionId { get; set; }
+
+        /// <summary>
+        /// 目标id
+        /// </summary>
         public int? GoalId { get; set; }
+
+        /// <summary>
+        /// 用户id
+        /// </summary>
         public int? UserId { get; set; }
+
+        /// <summary>
+        /// 是否启用
+        /// </summary>
         public sbyte? AttentionIsUse { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
         public DateTime? AttentionCreateTime { get; set; }
     }
 }
