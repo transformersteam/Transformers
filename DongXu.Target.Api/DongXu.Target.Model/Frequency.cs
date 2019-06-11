caf0056bb717ae37ac08e03cc0984cdd13721cfb
张三 <649292461@qq.com>
2019-06-11 17:19:25 +0800

    Update Frequency.cs
    牛的

--- DongXu.Target.Api/DongXu.Target.Model/Frequency.cs
+++ DongXu.Target.Api/DongXu.Target.Model/Frequency.cs
@@ -5,6 +5,9 @@ namespace DongXu.Target.Model
 {
     public partial class Frequency
     {
+        /// <summary>
+        /// 主键id
+        /// </summary>
         public int FrequencyId { get; set; }
         public string FrequencyName { get; set; }
         public DateTime? FrequencyCreateTime { get; set; }
