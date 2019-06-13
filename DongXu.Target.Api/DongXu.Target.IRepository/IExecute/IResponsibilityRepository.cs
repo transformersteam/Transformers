d0e4dcc27701a7fc89b67c2790d0762e3c1de51b
318900260@qq.com <Cxycxy123>
2019-06-13 19:00:48 +0800

    11

---
+++ DongXu.Target.Api/DongXu.Target.IRepository/IExecute/IResponsibilityRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DongXu.Target.Model;
+using DongXu.Target.Model.Dto;
+
+namespace DongXu.Target.IRepository.IExecute
+{
+    public interface IResponsibilityRepository
+    {
+        /// <summary>
+        /// 待办事项集合
+        /// </summary>
+        /// <returns></returns>
+        List<MainResponsibility> GetMainResponsibilityInfo();
+
+        /// <summary>
+        /// 待办事项显示
+        /// </summary>
+        /// <returns></returns>
+        MainResponsibilityPageination GetMainResponsibilityList(int pageindex = 1, int pagesize = 3);
+    } 
+}
