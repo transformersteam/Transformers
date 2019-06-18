0008d2409ee7023577fb795eda9f25b0531b1984
nidebinggege <374407194@qq.com>
2019-06-18 16:46:16 +0800

    常珂

---
+++ DongXu.Target.Api/DongXu.Target.Model/Dto/TrafficLight.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DongXu.Target.Model.Dto
+{
+    public class TrafficLight 
+    {
+        /// <summary>
+        /// 公司名称
+        /// </summary>
+        public string Role_Name { get; set; }
+
+        /// <summary>
+        /// 红灯
+        /// </summary>
+        public decimal RedLight { get; set; }
+
+        /// <summary>
+        /// 黄灯
+        /// </summary>
+        public decimal YellowLight { get; set; }
+
+        /// <summary>
+        /// 绿灯 
+        /// </summary>
+        public decimal GreenLight { get; set; } 
+
+    }
+}
