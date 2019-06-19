5fd69f46eb4be1b36341d3d62bf6969c5d5e9ed7
318900260@qq.com <Cxycxy123>
2019-06-19 21:47:16 +0800

    蔡新元

---
+++ DongXu.Target.Web/DongXu.Target.Web/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DongXu.Target.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public int acc()
+        {
+
+            HelperHttpClient.GetAll("post", "GoalManage/GoalAdd", "333");
+
+            return 1;
+        }
+
+    }
+}
