a0c45d174f3dcbca4cf8f27052d4ef0f458d7098
nidebinggege <374407194@qq.com>
2019-06-26 10:37:38 +0800

    测试

---
+++ DongXu.Target.Api/DongXu.Target.Test/GoalTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using DongXu.Target.Api.Controllers.ExecuteController;
+using DongXu.Target.Repository.Execute;
+using DongXu.Target.Model.Dto;
+
+namespace DongXu.Target.Test
+{
+    [TestClass]
+    public class GoalTest
+    {
+        private GoalRepository goal = new GoalRepository();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var list = goal.GetGoalList(1);
+            Assert.IsNotNull(list);
+        }
+    }
+}
